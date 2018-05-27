e347bfe49dccd6b3fe89a434b69442688890ceaa
jatin821 <wadhwajatin821@gmail.com>
2018-05-27 16:53:09 +0530

    edit update and destroy docs views changed and lil logic in controller

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,6 +1,7 @@
 class DocsController < ApplicationController
  before_action :find_doc, only: [:show, :edit, :update, :destroy]
 
+
 	def index
 		@docs = Doc.all.order("created_at DESC")
 	end
@@ -8,9 +9,11 @@ class DocsController < ApplicationController
 	def show
 	end
 
+
 	 def new
 	 	@doc = Doc.new
 	 end
+
 
 	 def create
        @doc = Doc.new(doc_params)
@@ -22,15 +25,31 @@ class DocsController < ApplicationController
        end
 
 	 end
+
+
 # no view for edit
 	 def edit
-	 end
-# no view for update 
-	 def update  
+
 	 end
 
-	 def destroy
+
+# no view for update 
+	 def update  
+	 	if @doc.update(doc_params)
+	 		redirect_to @doc
+	 	else 
+	 	#redirect is new http request we dont want it and iit delete evertjig on the page 
+	 		render 'edit' 
+	 	end
 	 end
+
+
+	 def destroy
+	 	@doc.destroy
+	 	redirect_to docs_path
+	 end
+
+
 
 	 private
 # used in edit update and destroy so keep it in private bcz it is used agai and again
