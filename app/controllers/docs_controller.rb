b91a9bf8a03cde0afc92bdd6fcc4e83c8bccc6bd
jatin821 <wadhwajatin821@gmail.com>
2018-05-28 21:44:15 +0530

    assoc btwuseranddoc user created for each doc

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -11,12 +11,13 @@ class DocsController < ApplicationController
 
 
 	 def new
-	 	@doc = Doc.new
+	 	@doc = current_user.docs.build
+
 	 end
 
 
 	 def create
-       @doc = Doc.new(doc_params)
+       @doc = current_user.docs.build(doc_params)
 
        if @doc.save
        	redirect_to @doc
