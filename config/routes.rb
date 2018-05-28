81002ca7f1ae7a79f2284011331f0c6ef1bb10fb
jatin821 <wadhwajatin821@gmail.com>
2018-05-28 22:07:04 +0530

    root changed for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   root 'welcome#index'
+  authenticated :user do
+  	#you cant have two root directory so we rename it as something else by using as 
+  	root"docs#index" , as: "authenticated_root"
+  end
+
   resources :docs
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
